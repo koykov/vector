9c5197c77b829ed56d76a2cd5d1a30e185616169
Konstantin Voykov <koykov0@gmail.com>
2021-10-11 22:34:39 +0300

    Code fmt issue.

--- indirect_amd64.go
+++ indirect_amd64.go
@@ -1,4 +1,7 @@
 package vector
 
+// Indirect vector object from raw pointer.
+//
+// Fix "checkptr: pointer arithmetic result points to invalid allocation" error in race mode.
 //go:noescape
 func indirectVector1(_ uintptr) *Vector
