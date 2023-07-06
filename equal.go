b4aff93cd7fa95118abf2bc3bfd03eff9b7d6598
Konstantin Voykov <koykov0@gmail.com>
2023-07-06 21:41:46 +0300

    Implement equal methods.

---
+++ equal.go
@@ -0,0 +1,61 @@
+package vector
+
+import "bytes"
+
+// EqualWith compares vector with exp.
+func (vec *Vector) EqualWith(exp *Vector) bool {
+	return equal(vec.Root(), exp.Root())
+}
+
+// EqualWith compares node with exp.
+func (n *Node) EqualWith(exp *Node) bool {
+	return equal(n, exp)
+}
+
+func equal(a, b *Node) bool {
+	switch {
+	case a == nil && b == nil:
+		return true
+	case a == nil:
+		return false
+	case b == nil:
+		return false
+	}
+	if a.Type() != b.Type() {
+		return false
+	}
+	if a.Limit() != b.Limit() {
+		return false
+	}
+	ok := true
+	switch a.Type() {
+	case TypeObj:
+		a.Each(func(_ int, ac *Node) {
+			bc := b.Get(ac.KeyString())
+			if bc.Type() == TypeNull {
+				ok = false
+				return
+			}
+			if !equal(ac, bc) {
+				ok = false
+				return
+			}
+		})
+	case TypeArr:
+		a.Each(func(idx int, ac *Node) {
+			bc := b.At(idx)
+			if !equal(ac, bc) {
+				ok = false
+				return
+			}
+		})
+	case TypeUnk:
+	case TypeNull:
+		return true
+	default:
+		if !bytes.Equal(a.Value().RawBytes(), b.Value().RawBytes()) {
+			return false
+		}
+	}
+	return ok
+}
