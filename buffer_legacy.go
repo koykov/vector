52dce4db915a450b3601eb5a0a3587d600f0f322
Konstantin Voykov <koykov0@gmail.com>
2024-08-17 00:12:00 +0300

    Legacy: fmt issues.

--- buffer_legacy.go
+++ buffer_legacy.go
@@ -2,40 +2,6 @@ package vector
 
 import "strconv"
 
-// Legacy types.
-const (
-	// TypeUnk is a legacy version of TypeUnknown.
-	// DEPRECATED: use TypeUnknown instead.
-	TypeUnk Type = 0
-	// TypeObj is a legacy version of TypeObject.
-	// DEPRECATED: use TypeObject instead.
-	TypeObj Type = 1
-	// TypeArr is a legacy version of TypeArray.
-	// DEPRECATED: use TypeArray instead.
-	TypeArr Type = 3
-	// TypeStr is a legacy version of TypeString.
-	// DEPRECATED: use TypeString instead.
-	TypeStr Type = 4
-	// TypeNum is a legacy version of TypeNumber.
-	// DEPRECATED: use TypeNumber instead.
-	TypeNum Type = 5
-	// TypeAttr is a legacy version of TypeAttribute.
-	// DEPRECATED: use TypeAttribute instead.
-	TypeAttr Type = 7
-)
-
-// ParseStr is a legacy version of ParseString.
-// DEPRECATED: use ParseString instead.
-func (vec *Vector) ParseStr(_ string) error {
-	return ErrNotImplement
-}
-
-// ParseCopyStr is a legacy version of ParseCopyString.
-// DEPRECATED: use ParseCopyString instead.
-func (vec *Vector) ParseCopyStr(_ string) error {
-	return ErrNotImplement
-}
-
 // BufUpdateWith is a legacy version of BufReplaceWith.
 // DEPRECATED: use BufReplaceWith instead.
 func (vec *Vector) BufUpdateWith(b []byte) {
