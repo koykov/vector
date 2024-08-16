f754fbae1a063683b9b130a9e662207ce4ade04b
Konstantin Voykov <koykov0@gmail.com>
2024-08-17 00:03:29 +0300

    Buffer methods: mark legacy.

--- buffer.go
+++ buffer.go
@@ -2,8 +2,6 @@ package vector
 
 import (
 	"strconv"
-
-	"github.com/koykov/byteconv"
 )
 
 // BufLen returns length of buffer.
@@ -35,44 +33,43 @@ func (vec *Vector) Bufferize(b []byte) []byte {
 }
 
 // BufferizeString appends string to internal buffer and returns buffered value.
-func (vec *Vector) BufferizeString(s string) string {
+func (vec *Vector) BufferizeString(s string) []byte {
 	off := vec.BufLen()
 	vec.buf = append(vec.buf, s...)
-	return byteconv.B2S(vec.buf[off:])
+	return vec.buf[off:]
 }
 
-// BufAppend appends bytes to the buffer.
-// DEPRECATED: use Bufferize instead.
-func (vec *Vector) BufAppend(s []byte) {
-	vec.buf = append(vec.buf, s...)
-}
-
-// BufAppendStr appends string to the buffer.
-func (vec *Vector) BufAppendStr(s string) {
-	vec.buf = append(vec.buf, s...)
-}
-
-// BufAppendByte appends single byte to the buffer.
-func (vec *Vector) BufAppendByte(b byte) {
+// BufferizeByte appends b to internal buffer and returns buffered value.
+func (vec *Vector) BufferizeByte(b byte) []byte {
+	off := vec.BufLen()
 	vec.buf = append(vec.buf, b)
+	return vec.buf[off:]
 }
 
-// BufAppendInt appends int to the buffer.
-func (vec *Vector) BufAppendInt(i int64) {
+// BufferizeInt appends integer to internal buffer and returns buffered value.
+func (vec *Vector) BufferizeInt(i int64) []byte {
+	off := vec.BufLen()
 	vec.buf = strconv.AppendInt(vec.buf, i, 10)
+	return vec.buf[off:]
 }
 
-// BufAppendUint appends uint to the buffer.
-func (vec *Vector) BufAppendUint(u uint64) {
+// BufferizeUint appends unsigned integer to internal buffer and returns buffered value.
+func (vec *Vector) BufferizeUint(u uint64) []byte {
+	off := vec.BufLen()
 	vec.buf = strconv.AppendUint(vec.buf, u, 10)
+	return vec.buf[off:]
 }
 
-// BufAppendFloat appends float to the buffer.
-func (vec *Vector) BufAppendFloat(f float64) {
+// BufferizeFloat appends unsigned integer to internal buffer and returns buffered value.
+func (vec *Vector) BufferizeFloat(f float64) []byte {
+	off := vec.BufLen()
 	vec.buf = strconv.AppendFloat(vec.buf, f, 'f', -1, 64)
+	return vec.buf[off:]
 }
 
-// BufAppendFloatTune appends float with extended params to the buffer.
-func (vec *Vector) BufAppendFloatTune(f float64, fmt byte, prec, bitSize int) {
+// BufferizeFloatTune appends float to internal buffer and returns buffered value.
+func (vec *Vector) BufferizeFloatTune(f float64, fmt byte, prec, bitSize int) []byte {
+	off := vec.BufLen()
 	vec.buf = strconv.AppendFloat(vec.buf, f, fmt, prec, bitSize)
+	return vec.buf[off:]
 }
--- legacy.go
+++ legacy.go
@@ -1,5 +1,7 @@
 package vector
 
+import "strconv"
+
 // Legacy types.
 const (
 	// TypeUnk is a legacy version of TypeUnknown.
@@ -32,4 +34,46 @@ func (vec *Vector) ParseStr(_ string) error {
 // DEPRECATED: use ParseCopyString instead.
 func (vec *Vector) ParseCopyStr(_ string) error {
 	return ErrNotImplement
+}
+
+// BufAppend appends bytes to the buffer.
+// DEPRECATED: use Bufferize instead.
+func (vec *Vector) BufAppend(s []byte) {
+	vec.buf = append(vec.buf, s...)
+}
+
+// BufAppendStr appends string to the buffer.
+// DEPRECATED: use BufferizeString instead.
+func (vec *Vector) BufAppendStr(s string) {
+	vec.buf = append(vec.buf, s...)
+}
+
+// BufAppendByte appends single byte to the buffer.
+// DEPRECATED: use BufferizeByte instead.
+func (vec *Vector) BufAppendByte(b byte) {
+	vec.buf = append(vec.buf, b)
+}
+
+// BufAppendInt appends int to the buffer.
+// DEPRECATED: use BufferizeInt instead.
+func (vec *Vector) BufAppendInt(i int64) {
+	vec.buf = strconv.AppendInt(vec.buf, i, 10)
+}
+
+// BufAppendUint appends uint to the buffer.
+// DEPRECATED: use BufferizeUint instead.
+func (vec *Vector) BufAppendUint(u uint64) {
+	vec.buf = strconv.AppendUint(vec.buf, u, 10)
+}
+
+// BufAppendFloat appends float to the buffer.
+// DEPRECATED: use BufferizeFloat instead.
+func (vec *Vector) BufAppendFloat(f float64) {
+	vec.buf = strconv.AppendFloat(vec.buf, f, 'f', -1, 64)
+}
+
+// BufAppendFloatTune appends float with extended params to the buffer.
+// DEPRECATED: use BufferizeFloatTune instead.
+func (vec *Vector) BufAppendFloatTune(f float64, fmt byte, prec, bitSize int) {
+	vec.buf = strconv.AppendFloat(vec.buf, f, fmt, prec, bitSize)
 }
