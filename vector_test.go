297385d0af2378655110b18579ff2654415fd26a
Konstantin Voykov <koykov0@gmail.com>
2026-07-06 23:27:35 +0300

    populate: test draft

--- vector_test.go
+++ vector_test.go
@@ -1,5 +1,34 @@
 package vector
 
-import "sync"
+import (
+	"sync"
+	"testing"
+)
 
 var testPool = sync.Pool{New: func() any { return &Vector{} }}
+
+func TestVector(t *testing.T) {
+	t.Run("populate", func(t *testing.T) {
+		var vec Vector
+		_ = vec.SetSrc([]byte("zzz"), false)
+		r, ri := vec.AcquireNode(0)
+		r.SetType(TypeObject)
+
+		n, i := vec.AcquireChildWithType(r, 1, TypeString)
+		n.Key().InitString("foo", 0, 3)
+		n.Value().InitString("qwerty", 0, 6)
+		r.ReleaseChild(i, n)
+
+		n, i = vec.AcquireChildWithType(r, 1, TypeString)
+		n.Key().InitString("bar", 0, 3)
+		n.Value().InitString("asdfgh", 0, 6)
+		r.ReleaseChild(i, n)
+
+		vec.ReleaseNode(ri, r)
+
+		m := make(map[string]any)
+		vec.Populate(m)
+
+		// assert m
+	})
+}
